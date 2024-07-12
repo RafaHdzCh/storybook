ae7fd8c17f9f5728b3995d3138a83d240724e85a
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-12 15:47:20 -0600

    Button to jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,21 @@
 const button = "border-none text-white py-3 px-8 text-center no-underline inline-block text-base rounded cursor-pointer";
 
-export const primary = "bg-cyan-500";
-export const success = "bg-green-500";
-export const danger = "bg-red-500";
-export const secondary = "bg-cyan-100 text-black";
+export const variants =
+{
+  primary: "bg-cyan-500",
+  success: "bg-green-500",
+  danger: "bg-red-500",
+  secondary: "bg-cyan-100 text-black",
+}
 
-export const sizes = {
+export const sizes = 
+{
   small: "py-2 px-4 text-sm",
   medium: "py-3 px-8 text-base",
   large: "py-4 px-12 text-lg",
 };
 
-export default function Button({children, variant = primary, size = sizes.medium}) 
+export default function Button({children, variant = variants.primary, size = sizes.medium}) 
 {
   return (
     <button className={`${button} ${variant} ${size}`}>
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Button, {primary, secondary, success, danger, sizes} from "./Button";
+import Button, {variants, sizes} from "./Button";
 
 export default 
 {
@@ -7,18 +7,18 @@ export default
   component: Button,
 };
 
-export const PrimarySmall = () => <Button variant={primary} size={sizes.small}>Primary Small</Button>;
-export const PrimaryMedium = () => <Button variant={primary} size={sizes.medium}>Primary Medium</Button>;
-export const PrimaryLarge = () => <Button variant={primary} size={sizes.large}>Primary Large</Button>;
+export const PrimarySmall = () => <Button variant={variants.primary} size={sizes.small}>Primary Small</Button>;
+export const PrimaryMedium = () => <Button variant={variants.primary} size={sizes.medium}>Primary Medium</Button>;
+export const PrimaryLarge = () => <Button variant={variants.primary} size={sizes.large}>Primary Large</Button>;
 
-export const SecondarySmall = () => <Button variant={secondary} size={sizes.small}>Secondary Small</Button>;
-export const SecondaryMedium = () => <Button variant={secondary} size={sizes.medium}>Secondary Medium</Button>;
-export const SecondaryLarge = () => <Button variant={secondary} size={sizes.large}>Secondary Large</Button>;
+export const SecondarySmall = () => <Button variant={variants.secondary} size={sizes.small}>Secondary Small</Button>;
+export const SecondaryMedium = () => <Button variant={variants.secondary} size={sizes.medium}>Secondary Medium</Button>;
+export const SecondaryLarge = () => <Button variant={variants.secondary} size={sizes.large}>Secondary Large</Button>;
 
-export const SuccessSmall = () => <Button variant={success} size={sizes.small}>Success Small</Button>;
-export const SuccessMedium = () => <Button variant={success} size={sizes.medium}>Success Medium</Button>;
-export const SuccessLarge = () => <Button variant={success} size={sizes.large}>Success Large</Button>;
+export const SuccessSmall = () => <Button variant={variants.success} size={sizes.small}>Success Small</Button>;
+export const SuccessMedium = () => <Button variant={variants.success} size={sizes.medium}>Success Medium</Button>;
+export const SuccessLarge = () => <Button variant={variants.success} size={sizes.large}>Success Large</Button>;
 
-export const DangerSmall = () => <Button variant={danger} size={sizes.small}>Danger Small</Button>;
-export const DangerMedium = () => <Button variant={danger} size={sizes.medium}>Danger Medium</Button>;
-export const DangerLarge = () => <Button variant={danger} size={sizes.large}>Danger Large</Button>;
+export const DangerSmall = () => <Button variant={variants.danger} size={sizes.small}>Danger Small</Button>;
+export const DangerMedium = () => <Button variant={variants.danger} size={sizes.medium}>Danger Medium</Button>;
+export const DangerLarge = () => <Button variant={variants.danger} size={sizes.large}>Danger Large</Button>;
