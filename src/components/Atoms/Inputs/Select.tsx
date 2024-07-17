b513292948bf332fb90dd29bfd8843f595eb719c
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-17 12:00:28 -0600

    Select

---
+++ src/components/Atoms/Inputs/Select.tsx
@@ -0,0 +1,12 @@
+import { Select as Selector, useId, SelectProps } from "@fluentui/react-components";
+import * as React from "react";
+
+export const SelectOption = (props: SelectProps) => {
+  const selectId = useId();
+
+  return (
+    <div className="max-w-sm mx-auto p-4">
+      <Selector id={selectId} {...props} />
+    </div>
+  );
+};
---
+++ src/stories/Atoms/Inputs/Select.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { Select, SelectProps } from "@fluentui/react-components";
+import { useId } from "@fluentui/react-hooks";
+import { SelectOption } from '../../../components/Atoms/Inputs/Select';
+
+export default {
+  title: 'Atoms/Inputs/Select',
+  component: Select,
+} as Meta;
+
+interface TemplateProps extends SelectProps {
+  options?: string[];
+}
+
+const Template: StoryFn<TemplateProps> = ({ options = [], ...args }) => {
+  const selectId = useId();
+
+  return (
+      <SelectOption id={selectId} {...args}>
+        {options.map((option, index) => (
+          <option key={index}>{option}</option>
+        ))}
+      </SelectOption>
+  );
+};
+
+
+export const Default = Template.bind({});
+Default.args = {
+  options: ['Red', 'Green', 'Blue']
+};
