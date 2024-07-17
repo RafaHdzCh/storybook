aa996807af5678f4dc4a8d29fccb2eccf1879ac0
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-17 14:51:13 -0600

    Progress Bar - WIP

--- src/components/Atoms/General/ProgressBar.tsx
+++ src/components/Atoms/General/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import { Field,ProgressBar} from "@fluentui/react-components";
+import { Field } from "@fluentui/react-components";
 import * as React from "react";
 
 const styles = `
@@ -6,26 +6,54 @@ const styles = `
   mb-0
 `;
 
-export const Shape = () => 
-{
+type ProgressBarProps = {
+  className?: string;
+  shape: 'rounded' | 'square';
+  thickness: 'small' | 'large';
+  value: number;
+};
+
+export const ProgressBar = ({ className, shape, thickness, value }: ProgressBarProps) => {
+  const containerClasses = `
+    ${className || ''}
+    ${shape === 'rounded' ? 'rounded-full' : ''}
+    bg-blue-500
+    ${thickness === 'small' ? 'h-2' : 'h-4'}
+    relative
+    overflow-hidden
+  `;
+
+  const progressWidth = `${value}%`;
+
+  return (
+    <div className={containerClasses}>
+      <div className="absolute top-0 left-0 bg-white" style={{ width: progressWidth }} />
+    </div>
+  );
+};
+
+export const Shape = () => {
+  const [progressValue, setProgressValue] = React.useState(50); // Valor inicial
+
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value, 10);
+    setProgressValue(newValue);
+  };
+
   return (
     <div>
       <Field validationMessage="Rounded ProgressBar" validationState="none">
-        <ProgressBar
-          className={styles}
-          shape="rounded"
-          thickness="large"
-          value={0.5}
-        />
+        <ProgressBar className={styles} shape="rounded" thickness="large" value={progressValue} />
       </Field>
-      <Field validationMessage="Square ProgressBar" validationState="none">
-        <ProgressBar
-          className={styles}
-          shape="square"
-          thickness="large"
-          value={0.5}
-        />
-      </Field>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={progressValue}
+        onChange={handleSliderChange}
+        style={{ width: '200px', marginTop: '10px' }}
+      />
+      <p>Valor: {progressValue}</p>
     </div>
   );
 };
--- src/stories/Atoms/General/ProgressBar.stories.tsx
+++ src/stories/Atoms/General/ProgressBar.stories.tsx
@@ -1,28 +1,23 @@
-import { Meta, StoryFn } from '@storybook/react';
-import { ProgressBar } from '@fluentui/react-components';
 import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { ProgressBar } from "../../../components/Atoms/General/ProgressBar";
 
 export default {
   title: 'Atoms/General/ProgressBar',
   component: ProgressBar,
 } as Meta;
 
-const Template: StoryFn<{ shape: 'rounded' | 'square'; thickness: 'large'; value: number }> = (args) => (
-  <div className="m-4">
-    <ProgressBar {...args} className="rounded-full bg-blue-500 h-4" />
-  </div>
-);
+type ProgressBarProps = {
+  shape: 'rounded' | 'square';
+  thickness: 'small' | 'large';
+  value: number;
+};
+
+const Template: StoryFn<ProgressBarProps> = (args) => <ProgressBar {...args} />;
 
 export const RoundedProgressBar = Template.bind({});
 RoundedProgressBar.args = {
   shape: 'rounded',
   thickness: 'large',
-  value: 0.5,
-};
-
-export const SquareProgressBar = Template.bind({});
-SquareProgressBar.args = {
-  shape: 'square',
-  thickness: 'large',
-  value: 0.5,
+  value: 50, // Valor inicial del slider
 };
