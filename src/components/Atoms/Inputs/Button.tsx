7654241687c75b3738b741407b5370af507ef3e4
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-18 09:51:18 -0600

    Button - Styles WIP

--- src/components/Atoms/Inputs/Button.tsx
+++ src/components/Atoms/Inputs/Button.tsx
@@ -1,148 +1,90 @@
 import React, { useState } from 'react'
 
 interface ButtonProps {
-  children: string;
+  Text: string;
   bgColor?: string;
   bgHover?: string;
   textColor?: string;
   textHover?: string;
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   icon?: any;
-  style?: any;
+  shadow?: boolean;
   loading?: boolean;
   disabled?: boolean;
-  className?: string;
-  weight?: number;
+  weight?: 'font-thin' | 'font-light' | 'font-normal' | 'font-medium' | 'font-semibold' | 'font-bold' | 'font-extrabold' | 'font-black';
   outlined?: boolean;
   onClick?: (ev: any) => void;
 }
 
-export const Button = ({ style, children, outlined, bgColor = 'rgb(245, 158, 11)', bgHover, weight = 500, className, size = 'sm', icon, loading, disabled, textColor = 'white', textHover = 'white', onClick }: ButtonProps) => {
-  const [hoverStyle, setHoverSyle] = useState<boolean>(false)
+const sizeClasses = {
+  xs: 'px-4 text-xs',
+  sm: 'px-5 text-sm',
+  md: 'px-6 text-base',
+  lg: 'px-8 text-lg',
+  xl: 'px-8 text-xl'
+}
 
-  const handleMouseEnter = () => {
-    setHoverSyle(true)
+export const Button = (
+  { Text: children, 
+    outlined,
+    bgColor, 
+    bgHover, 
+    weight, 
+    size, 
+    icon, 
+    loading, 
+    disabled, 
+    shadow, 
+    textColor, 
+    textHover, 
+    onClick }: ButtonProps) => 
+  {
+  const [hoverStyle, SetHoverSyle] = useState<boolean>(false)
+
+  const HandleMouseEnter = () => 
+  {
+    SetHoverSyle(true)
   }
 
-  const handleMouseLeave = () => {
-    setHoverSyle(false)
+  const HandleMouseLeave = () => 
+  {
+    SetHoverSyle(false)
   }
 
-  let fontSize
-  let lineHeight
-  let padding = {}
-
-  switch (size) {
-    case 'xs':
-      fontSize = '12px'
-      lineHeight = '16px'
-      padding = {
-        paddingLeft: '1rem',
-        paddingRight: '1rem'
-      }
-      break
-
-    case 'sm':
-      padding = {
-        paddingLeft: '1.25rem',
-        paddingRight: '1.25rem'
-      }
-      fontSize = '14px'
-      lineHeight = '18px'
-      break
-
-    case 'md':
-      padding = {
-        paddingLeft: '1.50rem',
-        paddingRight: '1.50rem'
-      }
-      fontSize = '16px'
-      lineHeight = '24px'
-      break
-
-    case 'lg':
-      padding = {
-        paddingLeft: '2rem',
-        paddingRight: '2rem'
-      }
-      fontSize = '18px'
-      lineHeight = '28px'
-      break
-
-    case 'xl':
-      padding = {
-        paddingLeft: '2rem',
-        paddingRight: '2rem'
-      }
-      fontSize = '20px'
-      lineHeight = '28px'
-      break
-
-    default:
-      break
-  }
-
-  let styles = {
-    ...padding,
-    color: hoverStyle ? textHover : textColor,
-    backgroundColor: hoverStyle ? (bgHover ?? bgColor) : bgColor,
-    fontWeight: weight,
-    fontSize,
-    lineHeight,
-    border: ''
-  }
-
-  let disabledStyles = {
-    ...padding,
-    color: 'rgb(156 163 175)',
-    backgroundColor: 'rgb(209 213 219)',
-    fontWeight: weight,
-    fontSize,
-    lineHeight,
-    border: ''
-  }
-
-  if (outlined) {
-    styles = {
-      ...padding,
-      color: hoverStyle ? textHover : textColor,
-      backgroundColor: hoverStyle ? 'rgba(102, 102, 102, 0.08)' : 'transparent',
-      fontWeight: weight,
-      fontSize,
-      lineHeight,
-      border: '2px solid ' + (hoverStyle ? bgHover : bgColor)
-    }
-    disabledStyles = {
-      ...padding,
-      color: 'rgb(156 163 175)',
-      backgroundColor: 'rgba(199, 199, 199, 0.4)',
-      fontWeight: weight,
-      fontSize,
-      lineHeight,
-      border: '2px solid rgb(209 213 219)'
-    }
-  }
+  const styles = `
+    
+    ${size ? `${sizeClasses[size]}` : `${sizeClasses["sm"]}`}
+    ${weight ? `${weight}` : 'font-medium'}
+    ${textColor ? `${textColor}` : 'text-gray-950'}
+    ${textHover ? `${textHover}` : 'text-gray-950'}
+    ${bgColor ? `${bgColor}` : "'bg-yellow-500'"}
+    ${hoverStyle ? `${bgHover}` : "'bg-yellow-500'"}
+    ${outlined ? "border-2 border-gray-950" : "border-none"}
+    ${disabled || loading ? 'cursor-not-allowed text-gray-50 bg-gray-200 hover:text-gray-50' : ''}
+    ${shadow ? 'shadow-md' : ''}
+    rounded
+    transition-all
+    duration-200
+    ease-in-out
+    flex
+    items-center
+    justify-center
+  `
 
   return (
     <button
-    style={!(disabled || loading) ? { ...styles, ...style } : { ...disabledStyles, ...style }}
-    onMouseEnter={handleMouseEnter}
-    onMouseLeave={handleMouseLeave}
-    className={`
-    ${className} 
-    button
-    z-depth-1
-    montserrat
-    ${(disabled || loading) && ' cursor-not-allowed bg-gray-300 hover:bg-gray-300 text-gray-400 hover:text-gray-400'}
-    `}
-    onClick={(ev: any) => onClick && onClick(ev)}
-    disabled={(disabled || loading)}>
-        {
-        loading
-          ? <div className='spinner-xs mr-0.5 -ml-3'></div>
-          : icon && <span className='self-center'>{icon}</span>
-        }
-        <span className='self-center'>{children}</span>
+      className={styles}
+      onMouseEnter={HandleMouseEnter}
+      onMouseLeave={HandleMouseLeave}
+      onClick={(ev: any) => !disabled && !loading && onClick && onClick(ev)}
+      disabled={disabled || loading}
+    >
+      {loading ? (
+        <div className='spinner-xs mr-2'></div>
+      ) : (
+        icon && <span className='mr-2'>{icon}</span>
+      )}
+      <span>{children}</span>
     </button>
   )
 }
--- src/stories/Atoms/Inputs/Button.stories.tsx
+++ src/stories/Atoms/Inputs/Button.stories.tsx
@@ -6,103 +6,72 @@ import { Button } from '../../../components/Atoms/Inputs/Button'
 export default {
   title: 'Atoms/Inputs/Button',
   component: Button,
-  argTypes: {
-    bgHover: { control: 'color' },
-    textColor: { control: 'color' },
-    textHover: { control: 'color' },
+  argTypes: 
+  {
+    icon: { table: { disable: true } },
   },
 } as Meta<typeof Button>
 
 const Template: StoryFn<typeof Button> = (args) => <Button {...args} />
 
-export const NormalWithIcon = Template.bind({})
-NormalWithIcon.args = {
-  bgColor: 'rgb(245, 158, 11)',
-  bgHover: 'rgb(217, 119, 6)',
-  textColor: 'white',
-  textHover: 'white',
-  children: 'BUTTON',
-  disabled: false,
-  loading: false,
-  size: 'sm',
-  weight: 500,
-  icon: <MdAdd className='-ml-3 text-xl self-center' />
-}
-
 export const Normal = Template.bind({})
 Normal.args = {
-  bgColor: 'rgb(245, 158, 11)',
-  bgHover: 'rgb(217, 119, 6)',
-  textColor: 'white',
-  textHover: 'white',
+  bgColor: 'bg-yellow-500',
+  bgHover: 'bg-yellow-600',
+  textColor: 'text-gray-700',
+  textHover: "hover:text-gray-900",
+  shadow: true,
   disabled: false,
   loading: false,
+  outlined: false,
   size: 'sm',
-  weight: 500,
-  children: 'BUTTON'
-} 
-
-export const Outlined = Template.bind({})
-Outlined.args = {
-  bgColor: 'rgb(245, 158, 11)',
-  bgHover: 'rgb(217, 119, 6)',
-  textColor: 'rgb(245, 158, 11)',
-  textHover: 'rgb(217, 119, 6)',
-  disabled: false,
-  loading: false,
-  outlined: true,
-  size: 'sm',
-  children: 'BUTTON',
-  weight: 500,
-  icon: <MdAdd className='-ml-3 text-xl self-center' />
-} 
-
-export const OutlinedWithIcon = Template.bind({})
-OutlinedWithIcon.args = {
-  bgColor: 'rgb(245, 158, 11)',
-  bgHover: 'rgb(217, 119, 6)',
-  textColor: 'rgb(245, 158, 11)',
-  textHover: 'rgb(217, 119, 6)',
-  disabled: false,
-  loading: false,
-  outlined: true,
-  size: 'sm',
-  weight: 500,
-  children: 'BUTTON'
-} 
-
-export const Disabled = Template.bind({})
-Disabled.args = {
-  children: 'BUTTON',
-  weight: 500,
-  disabled: true
+  weight: 'font-semibold',
+  Text: 'NORMAL',
 }
 
-export const Loading = Template.bind({})
-Loading.args = {
-  children: 'BUTTON',
-  weight: 500,
-  loading: true,
-}
-
-export const OutlinedDisabled = Template.bind({})
-OutlinedDisabled.args = {
-  disabled: true,
-  weight: 500,
+export const NormalOutlined = Template.bind({})
+NormalOutlined.args = {
+  bgColor: 'bg-yellow-500',
+  bgHover: 'bg-yellow-600',
+  textColor: 'text-gray-700',
+  textHover: "hover:text-gray-900",
+  shadow: true,
+  disabled: false,
   loading: false,
   outlined: true,
   size: 'sm',
-  children: 'BUTTON',
+  weight: 'font-semibold',
+  Text: 'OUTLINED',
+}
+
+export const NormalIcon = Template.bind({})
+NormalIcon.args = {
+  bgColor: 'bg-yellow-500',
+  bgHover: 'bg-yellow-600',
+  textColor: 'text-gray-700',
+  textHover: "hover:text-gray-900",
+  shadow: true,
+  disabled: false,
+  loading: false,
+  outlined: false,
+  size: 'sm',
+  weight: 'font-semibold',
+  Text: 'ICON',
   icon: <MdAdd className='-ml-3 text-xl self-center' />
 }
 
-export const OutlinedLoading = Template.bind({})
-OutlinedLoading.args = {
+export const NormalOutlinedIcon = Template.bind({})
+NormalOutlinedIcon.args = {
+  bgColor: 'bg-yellow-500',
+  bgHover: 'bg-yellow-600',
+  textColor: 'text-gray-700',
+  textHover: "hover:text-gray-900",
+  shadow: true,
   disabled: false,
-  weight: 500,
-  loading: true,
+  loading: false,
   outlined: true,
   size: 'sm',
-  children: 'BUTTON',
+  weight: 'font-semibold',
+  Text: 'OUTLINED ICON',
   icon: <MdAdd className='-ml-3 text-xl self-center' />
-} 
+}
