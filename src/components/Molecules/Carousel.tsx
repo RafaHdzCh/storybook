518f12705aafc44a12199a7ae89812d3b2f8a8a2
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-19 09:35:44 -0600

    Carousel

---
+++ src/components/Molecules/Carousel.tsx
@@ -0,0 +1,118 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { MdArrowForwardIos } from 'react-icons/md';
+
+interface CarouselProps {
+  children: React.ReactNode[]; // Cambia aquí para aceptar un array de nodos
+  height: string;
+  width: string;
+  timer?: number;
+}
+
+export const Carousel: React.FC<CarouselProps> = ({ children, width, height, timer }) => {
+  const [countPages, setCount] = useState<number>(children.length); // Inicializa con la longitud de los hijos
+  const [currentPage, setPage] = useState<number>(1);
+  const [carouselWidth, setWidth] = useState<number>(1);
+  const [interval, setTimer] = useState<any>(timer ? () => {
+    let i = 1;
+    return setInterval(() => {
+      if (i === countPages) {
+        setPage(1);
+        i = 1;
+      } else {
+        setPage(i + 1);
+        i = i + 1;
+      }
+    }, timer);
+  } : null);
+
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const moveForward = () => {
+    currentPage === countPages
+      ? setPage(1)
+      : setPage(currentPage + 1);
+    resetInterval(currentPage);
+  };
+
+  const moveBackward = () => {
+    currentPage === 1
+      ? setPage(countPages)
+      : setPage(currentPage - 1);
+    resetInterval(currentPage - 1);
+  };
+
+  const jumpToPage = (page: number) => {
+    setPage(page);
+    resetInterval(page);
+  };
+
+  const resetInterval = (actualpage: number) => {
+    if (interval) clearInterval(interval);
+    timer && setTimer(
+      () => {
+        let i = actualpage;
+        return setInterval(() => {
+          if (i >= countPages) {
+            setPage(1);
+            i = 1;
+          } else {
+            setPage(i + 1);
+            i = i + 1;
+          }
+        }, timer);
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (contentRef.current) {
+      const childrenContent = contentRef.current.querySelectorAll('section');
+      for (let child of childrenContent) {
+        (child as HTMLElement).style.width = width;
+        (child as HTMLElement).style.height = height;
+        setWidth((child as HTMLElement).offsetWidth);
+      }
+      setCount(childrenContent.length);
+    }
+  }, [height, width, children]); // Agrega children a las dependencias
+
+  const renderDots = (currentPage: number) => {
+    let dots: React.ReactNode[] = [];
+    let styles;
+    for (let i = 1; i <= countPages; i++) {
+      if (currentPage === i) {
+        styles = {
+          backgroundColor: 'orange',
+        };
+      } else {
+        styles = {
+          backgroundColor: 'rgb(168 162 158)',
+        };
+      }
+      dots.push(
+        <div
+          key={i} // Agrega una clave única para cada dot
+          style={styles}
+          onClick={() => jumpToPage(i)}
+          className='text-center ease-out duration-100 rounded-full h-3 w-3 hover:bg-white hover:opacity-100 cursor-pointer'
+        />
+      );
+    }
+    return dots;
+  };
+
+  return (
+    <div className='overflow-hidden rounded bg-gray-300/25 relative' style={{ height, width: width }}>
+      <div ref={contentRef} style={{ transform: `translateX(-${carouselWidth * (currentPage - 1)}px)` }} className='transition ease-out duration-500 flex absolute flex-nowrap'>
+        {children}
+      </div>
+      <div style={{ width }} className='absolute bottom-4 flex justify-center space-x-3'>
+        {renderDots(currentPage)}
+      </div>
+      <div className='text-3xl text-gray-400'>
+        <MdArrowForwardIos onClick={() => moveBackward()} className='ml-4 absolute cursor-pointer h-full hover:text-gray-500 inset-y-0 left-0 rotate-180' />
+        <MdArrowForwardIos onClick={() => moveForward()} className='mr-4 absolute cursor-pointer h-full hover:text-gray-500 inset-y-0 right-0' />
+      </div>
+    </div>
+  );
+};
---
+++ src/stories/Molecules/Carousel.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { Carousel } from '../../components/Molecules/Carousel';
+
+export default {
+  title: 'Molecules/Carousel',
+  component: Carousel,
+} as Meta<typeof Carousel>;
+
+const Template: StoryFn<typeof Carousel> = (args) => <Carousel {...args} />;
+
+export const AutoSlide = Template.bind({});
+AutoSlide.args = {
+  timer: 4000,
+  height: '30vw',
+  width: '75vw',
+  children: [
+    <section key="1" className='grid place-content-center'>
+      <h3 className='text-xl font-bold'>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </section>,
+    <section key="2" className='grid place-content-center'>
+      <h3 className='text-xl font-bold'>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </section>,
+    <section key="3" className='grid place-content-center'>
+      <h3 className='text-xl font-bold'>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </section>,
+  ],
+};
+
+export const ManualSlide = Template.bind({});
+ManualSlide.args = {
+  height: '30vw',
+  width: '75vw',
+  children: [
+    <section key="1" className='grid place-content-center'>
+      <h3 className='text-2xl font-bold'>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </section>,
+    <section key="2" className='grid place-content-center'>
+      <h3 className='text-2xl font-bold'>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </section>,
+    <section key="3" className='grid place-content-center'>
+      <h3 className='text-2xl font-bold'>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </section>,
+  ],
+};
