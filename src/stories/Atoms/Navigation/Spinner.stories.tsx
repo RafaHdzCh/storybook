2b945d0181af1d2f568f46e270452749c4167c2b
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-22 10:22:41 -0600

    Spinner - base

---
+++ src/stories/Atoms/Navigation/Spinner.stories.tsx
@@ -0,0 +1,35 @@
+// src/components/Spinner/Spinner.stories.tsx
+
+import * as React from 'react';
+import { Spinner } from '@fluentui/react-components';
+import type { SpinnerProps } from '@fluentui/react-components';
+import { Meta, StoryFn } from '@storybook/react';
+
+export default {
+  title: 'Atoms/Navigation/Spinner',
+  component: Spinner,
+  parameters: {
+    docs: {
+      description: {
+        component: 'A Spinner component from Fluent UI for indicating loading states.',
+      },
+    },
+  },
+} as Meta;
+
+const Template: StoryFn<Partial<SpinnerProps>> = (args) => <Spinner {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  size: 'medium',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+};
