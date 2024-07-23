8747a387262f588a8d994e9e811d73e9c1953ea1
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-23 10:31:28 -0600

    Card - Translation

--- src/stories/Molecules/Card.stories.tsx
+++ src/stories/Molecules/Card.stories.tsx
@@ -21,7 +21,7 @@ FinanceExample.args = {
         <h1 className='text-3xl text-gray-600 text-center'>$3,200.00</h1>
         <p className='text-center antialiased mt-1 mb-2'>Fecha de vencimiento: 30/May/2022</p>
         <div className='flex justify-center'>
-            <Button label='Page' size='xl'/>
+            <Button label='Pagar' size='xl'/>
         </div>
     </div>
 }
@@ -32,7 +32,7 @@ ImageExample.args = {
     children: <div className='w-full'>
         <img className='rounded shadow-md' src="https://picsum.photos/200" alt="" />
         <div className='flex justify-center mt-2'>
-            <Button label='Download' size='xl'/>
+            <Button label='Descargar' size='xl'/>
         </div>
     </div> 
 }
