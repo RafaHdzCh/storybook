8222af87a2ce956b712b8067d9d0359e5a0f36d2
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-24 09:45:02 -0600

    Card - Styles

--- src/components/Molecules/Card.tsx
+++ src/components/Molecules/Card.tsx
@@ -3,15 +3,40 @@ import React from 'react'
 interface CardProps {
     title: string;
     children: any;
+    titleTextColor: string,
+    titleBgColor: string,
+    containerTextColor: string,
+    containerBgColor: string;
 }
 
-export const Card = ( { title, children }:CardProps ) => {
+export const Card = ( 
+  { 
+    title, 
+    children, 
+    titleTextColor, 
+    titleBgColor, 
+    containerTextColor, 
+    containerBgColor 
+  }:CardProps ) => 
+  {
+    
+  const titleContainerStyles = 
+  {
+    backgroundColor: titleBgColor,
+    color: titleTextColor,
+  };
+
+  const childrenContainerStyles = 
+  {
+    backgroundColor: containerBgColor,
+    color: containerTextColor,
+  };
   return (
-    <div className='w-full border border-gray-200 rounded-md shadow-sm '>
-        <div className='bg-gray-100 text-lg flex justify-center text-center p-3 px-6 leading-6 grow'>
+    <div className='w-full border rounded-md shadow-sm '>
+        <div style={titleContainerStyles} className='text-lg flex justify-center text-center p-3 px-6 leading-6 grow'>
             <h1 className='self-center antialiased'>{title}</h1>
         </div>
-        <section className='p-3 px-6 mt-2'>
+        <section style={childrenContainerStyles} className='p-3 px-6'>
             {children}
         </section>
     </div>
--- src/stories/Molecules/Card.stories.tsx
+++ src/stories/Molecules/Card.stories.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { StoryFn, Meta } from '@storybook/react'
 import { Card } from '../../components/Molecules/Card'
 import { Button } from '../../components/Atoms/Inputs/Button'
+import { ButtonVariants } from '../../stories/Atoms/Inputs/Button.stories';
 
 export default {
   title: 'Molecules/Card',
   component: Card,
+  argTypes:
+  {
+    children: { table: { disable: true } }
+  },
   decorators: [
     (Story) => <div style={ {maxWidth: '400px'} }>
         {Story()}
@@ -15,24 +20,24 @@ export default {
 const Template: StoryFn<typeof Card> = (args) => <Card {...args} />
 
 export const FinanceExample = Template.bind({})
-FinanceExample.args = {
-    title: 'Lorem ipsum dolor sit',
-    children: <div className='w-full'>
-        <h1 className='text-3xl text-gray-600 text-center'>$3,200.00</h1>
-        <p className='text-center antialiased mt-1 mb-2'>Fecha de vencimiento: 30/May/2022</p>
-        <div className='flex justify-center'>
-            <Button label='Pagar' size='xl'/>
-        </div>
-    </div>
-}
+FinanceExample.args = 
+{
+    title: 'Ficha de pago',
+    titleTextColor: "#4d7c0f", 
+    titleBgColor:"#ecfccb", 
+    containerTextColor:"#0e7490", 
+    containerBgColor:"#cffafe",
 
-export const ImageExample = Template.bind({})
-ImageExample.args = {
-    title: 'This is an image',
-    children: <div className='w-full'>
-        <img className='rounded shadow-md' src="https://picsum.photos/200" alt="" />
-        <div className='flex justify-center mt-2'>
-            <Button label='Descargar' size='xl'/>
+    children: (
+        <div className='w-full'>
+            <h1 className='text-3xl text-center'>$3,200.00</h1>
+            <div className='flex justify-center'>
+                <img className='rounded shadow-md my-4' src="https://picsum.photos/200" alt="" />
+            </div>
+            <p className='text-center antialiased mt-1 mb-2'>Fecha de vencimiento: 30/May/2022</p>
+            <div className='flex justify-center mt-4'>
+            <Button {...ButtonVariants.info} />
+            </div>
         </div>
-    </div> 
+    )
 }
