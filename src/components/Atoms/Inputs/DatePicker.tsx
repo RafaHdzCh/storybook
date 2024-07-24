24a2c83aa3f5670bb83d005500e7701dcd7c17e9
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-24 08:03:07 -0600

    DatePicker - Clean Template

--- src/components/Atoms/Inputs/DatePicker.tsx
+++ src/components/Atoms/Inputs/DatePicker.tsx
@@ -1,19 +1,6 @@
-import React, { useState } from "react";
-import { DayPicker } from "react-day-picker";
-import "react-day-picker/style.css";
+import React from "react";
 
 export default function DatePicker() 
 {
-  const [selected, setSelected] = useState<Date>();
-
-  return (
-    <DayPicker
-      mode="single"
-      selected={selected}
-      onSelect={setSelected}
-      footer={
-        selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
-      }
-    />
-  );
+  return (<React.Fragment></React.Fragment>);
 }
