9d38c9a6c461ad1edb583fcaabc38853d5496746
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-24 14:19:58 -0600

    DatePicker - More options

--- src/components/Atoms/Inputs/DatePicker.tsx
+++ src/components/Atoms/Inputs/DatePicker.tsx
@@ -1,3 +1,4 @@
+import { getDay } from "date-fns";
 import React, { useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -21,6 +22,31 @@ export function DayPicker()
       closeOnScroll={true}
       selected={startDate}
       className="border border-black shadow-md"
+      placeholderText="Select a range..."
+      onChange={(date: Date | null) => 
+      {
+        if (date) 
+        {
+          SetStartDate(date);
+        }
+      }}
+    />
+  );
+}
+
+export function DayPickerWithTime() 
+{
+  const [startDate, SetStartDate] = useState<Date>(new Date());
+
+  return (
+    <ReactDatePicker
+      closeOnScroll={true}
+      selected={startDate}
+      className="border border-black shadow-md"
+      placeholderText="Select a date..."
+      timeInputLabel="Time:"
+      dateFormat="MM/dd/yyyy h:mm aa"
+      showTimeInput
       onChange={(date: Date | null) => 
       {
         if (date) 
@@ -54,8 +80,61 @@ export function RangePicker() {
       startDate={startDate}
       endDate={endDate}
       className="border border-black shadow-md"
+      placeholderText="Select a range"
       onChange={handleDateChange}
       isClearable
+    />
+  );
+}
+
+export function WeekdayPicker() 
+{
+  const [startDate, setStartDate] = useState<Date | null>(null);
+
+  const isWeekday = (date: Date) => 
+  {
+    const day = getDay(date);
+    return day !== 0 && day !== 6;
+  };
+
+  return (
+    <ReactDatePicker
+      selected={startDate}
+      className="border border-black shadow-md"
+      filterDate={isWeekday}
+      placeholderText="Select a weekday"
+      onChange={(date: Date | null) => 
+      {
+        if (date) {
+          setStartDate(date);
+        }
+      }}
+    />
+  );
+}
+
+export function TimePicker() 
+{
+  const [startDate, SetStartDate] = useState<Date>(new Date());
+
+  return (
+    <ReactDatePicker
+      closeOnScroll={true}
+      showTimeSelect
+      showTimeSelectOnly
+      timeIntervals={15}
+      timeCaption="Time"
+      dateFormat="h:mm aa"
+      selected={startDate}
+      className="border border-black shadow-md"
+      placeholderText="Select a range..."
+      onChange={(date: Date | null) => 
+      {
+        if (date) 
+        {
+          SetStartDate(date);
+        }
+      }}
     />
   );
 }
--- src/stories/Atoms/Inputs/DatePicker.stories.tsx
+++ src/stories/Atoms/Inputs/DatePicker.stories.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { DayPicker, RangePicker } from "../../../components/Atoms/Inputs/DatePicker";
+import { DayPicker, RangePicker, DayPickerWithTime, WeekdayPicker,TimePicker } from "../../../components/Atoms/Inputs/DatePicker";
 
-export default {
+export default 
+{
   title: 'Atoms/Inputs/DatePicker',
   component: DayPicker,
 };
 
 export const DefaultDayPicker = () => <DayPicker />;
+export const DefaultTimePicker = () => <TimePicker />;
+export const DefaultDayPickerWithTime = () => <DayPickerWithTime />;
+export const DefaultWeekDayPicker = () => <WeekdayPicker />;
 export const DefaultRangePicker = () => <RangePicker />;
