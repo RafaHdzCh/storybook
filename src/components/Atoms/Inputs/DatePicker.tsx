3498f1e512c85ad4530774136cfd74185e79a98a
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-24 15:26:41 -0600

    DatePicker - WIP Disabled warnings

--- src/components/Atoms/Inputs/DatePicker.tsx
+++ src/components/Atoms/Inputs/DatePicker.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { getDay, setHours, setMinutes } from "date-fns";
 import React, { useState } from "react";
 import ReactDatePicker from "react-datepicker";
