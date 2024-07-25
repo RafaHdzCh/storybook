8780687ad91a0f993ace9d0c51adddfa148ea5f9
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-07-25 11:14:17 -0600

    ErrorMessage - Primary button

--- src/components/Atoms/General/ErrorMessage.tsx
+++ src/components/Atoms/General/ErrorMessage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '../Inputs/Button';
+import { ButtonVariants } from "../../../stories/Atoms/Inputs/Button.stories";
 
 export interface ErrorMessageProps 
 {
@@ -26,12 +27,10 @@ export const ErrorMessage: React.FC<ErrorMessageProps> = (
     textColor,
   }) => 
 {
-
   const pageStyle = {backgroundColor: bgColor};
   const modalStyle = {backgroundColor: bgModalColor};
   const titleStyle = {color: titleColor};
   const textStyle = {color: textColor};
-
 
   return (
     <div style={pageStyle} className="flex items-center justify-center h-screen w-screen">
@@ -42,9 +41,9 @@ export const ErrorMessage: React.FC<ErrorMessageProps> = (
         <p style={textStyle} className="py-4">{message}</p>
 
         <div className='flex items-center justify-center'>
-          <Button label={buttonLabel} />
+          <Button {...ButtonVariants.primary} label={buttonLabel} />
         </div>
-        
+
       </div>
     </div>
   );
