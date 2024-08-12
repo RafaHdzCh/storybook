49ab915cd2fa58d74cee469054dcdb86b9ad6c11
Rafael Hernandez Chavez <rafahdzcucs@gmail.com>
2024-08-12 10:08:41 -0600

    Table component

--- src/components/Atoms/General/Table.tsx
+++ src/components/Atoms/General/Table.tsx
@@ -1,97 +1,64 @@
 import * as React from "react";
-import * as ReactIcons from "@fluentui/react-icons";
-import * as RC from "@fluentui/react-components";
 
-const items = [
-  {
-    file: { label: "Meeting notes", icon: <ReactIcons.DocumentRegular /> },
-    author: { label: "Max Mustermann", status: "available" },
-    lastUpdated: { label: "7h ago", timestamp: 1 },
-    lastUpdate: {
-      label: "You edited this",
-      icon: <ReactIcons.EditRegular />,
-    },
-  },
-  {
-    file: { label: "Thursday presentation", icon: <ReactIcons.FolderRegular /> },
-    author: { label: "Erika Mustermann", status: "busy" },
-    lastUpdated: { label: "Yesterday at 1:45 PM", timestamp: 2 },
-    lastUpdate: {
-      label: "You recently opened this",
-      icon: <ReactIcons.OpenRegular />,
-    },
-  },
-  {
-    file: { label: "Training recording", icon: <ReactIcons.VideoRegular /> },
-    author: { label: "John Doe", status: "away" },
-    lastUpdated: { label: "Yesterday at 1:45 PM", timestamp: 2 },
-    lastUpdate: {
-      label: "You recently opened this",
-      icon: <ReactIcons.OpenRegular />,
-    },
-  },
-  {
-    file: { label: "Purchase order", icon: <ReactIcons.DocumentPdfRegular /> },
-    author: { label: "Jane Doe", status: "offline" },
-    lastUpdated: { label: "Tue at 9:30 AM", timestamp: 3 },
-    lastUpdate: {
-      label: "You shared this in a Teams chat",
-      icon: <ReactIcons.PeopleRegular />,
-    },
-  },
-];
+interface TableProps 
+{
+  headers: string[];
+  rows: string[][];
+  bgHeaderColor: string,
+  bgDarkColor: string, 
+  bgLightColor: string, 
+  textHeaderColor: string, 
+  textRowColor: string 
+}
 
-const columns = [
-  { columnKey: "file", label: "File" },
-  { columnKey: "author", label: "Author" },
-  { columnKey: "lastUpdated", label: "Last updated" },
-  { columnKey: "lastUpdate", label: "Last update" },
-];
-
-export const Table = () => {
+export const Table: React.FC<TableProps> = (
+  { 
+    headers, 
+    rows, 
+    bgHeaderColor, 
+    bgDarkColor, 
+    bgLightColor, 
+    textHeaderColor, 
+    textRowColor 
+  }) => 
+{
   return (
-    <RC.Table arial-label="Default table" style={{ minWidth: "510px" }}>
-      <RC.TableHeader>
-        <RC.TableRow>
-          {columns.map((column) => (
-            <RC.TableHeaderCell key={column.columnKey}>
-              {column.label}
-            </RC.TableHeaderCell>
+    <div>
+      <table className="table-auto w-full border-collapse border border-gray-200">
+        <thead>
+          <tr>
+            {
+              headers.map((header, index) => (
+                <th 
+                  key={index} 
+                  className="border border-gray-300 p-2"
+                  style={{color: textHeaderColor, backgroundColor: bgHeaderColor}}
+                >
+                  {header}
+                </th>
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody style={{color: textRowColor}}>
+          {rows.map((row, rowIndex) => (
+            <tr
+              key={rowIndex}
+              style={rowIndex % 2 === 0 ? {backgroundColor: bgLightColor} : {}}
+            >
+              {row.map((cell, cellIndex) => (
+                <td 
+                  key={cellIndex} 
+                  className="border border-gray-300 p-2"
+                  style={rowIndex % 2 !== 0 ? {backgroundColor: bgDarkColor} : {}}
+                >
+                  {cell}
+                </td>
+              ))}
+            </tr>
           ))}
-        </RC.TableRow>
-      </RC.TableHeader>
-      <RC.TableBody>
-        {items.map((item) => (
-          <RC.TableRow key={item.file.label}>
-            <RC.TableCell>
-              <RC.TableCellLayout media={item.file.icon}>
-                {item.file.label}
-              </RC.TableCellLayout>
-            </RC.TableCell>
-            <RC.TableCell>
-              <RC.TableCellLayout
-                media={
-                  <RC.Avatar
-                    aria-label={item.author.label}
-                    name={item.author.label}
-                    badge={{
-                      status: item.author.status as RC.PresenceBadgeStatus,
-                    }}
-                  />
-                }
-              >
-                {item.author.label}
-              </RC.TableCellLayout>
-            </RC.TableCell>
-            <RC.TableCell>{item.lastUpdated.label}</RC.TableCell>
-            <RC.TableCell>
-              <RC.TableCellLayout media={item.lastUpdate.icon}>
-                {item.lastUpdate.label}
-              </RC.TableCellLayout>
-            </RC.TableCell>
-          </RC.TableRow>
-        ))}
-      </RC.TableBody>
-    </RC.Table>
+        </tbody>
+      </table>
+    </div>
   );
 };
--- src/stories/Atoms/General/Table.stories.tsx
+++ src/stories/Atoms/General/Table.stories.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import { StoryFn, Meta } from '@storybook/react';
-import { Table } from "../../../components/Atoms/General/Table"
+import { Table } from '../../../components/Atoms/General/Table';
 
-export default {
+export default 
+{
   title: 'Atoms/General/Table',
   component: Table,
-  argTypes: 
-  {
-    
+  argTypes: {
+    headers: {
+      control: 'array',
+      description: 'Headers of the table',
+    },
+    rows: {
+      control: 'array',
+      description: 'Rows of the table',
+    },
   },
 } as Meta<typeof Table>;
 
-const Template: StoryFn<typeof Table> = () => <Table />;
+const Template: StoryFn<typeof Table> = (args) => <Table {...args} />;
 
 export const DefaultTable = Template.bind({});
-DefaultTable.args = {};
+DefaultTable.args = 
+{
+  headers: ["Company", "Contact", "Country"],
+  rows: 
+  [
+    ["Alfreds", "Maria Anders", "Germany"],
+    ["Centro comercial Moctezuma", "Francisco Chang", "Mexico"],
+    ["Ernst Handel", "Roland Mendel", "Austria"],
+    ["Island Trading", "Helen Bennett", "UK"],
+    ["Laughing Bacchus Winecellars", "Yoshi Tannamuri", "Canada"],
+    ["Magazzini Alimentari Riuniti", "Giovanni Rovelli", "Italy"],
+  ],
+  bgDarkColor: "#7dd3fc",
+  bgLightColor: "#bae6fd",
+  bgHeaderColor: "#38bdf8",
+  textRowColor: "#082f49",
+  textHeaderColor: "#f0f9ff",
+};
